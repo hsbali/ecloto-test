798a825ea2ca4f2d7fe4a92720606caf7e9506a2
Harmeet Singh Bali <hsbaliwork@gmail.com>
2025-08-20 02:33:40 +0530

    submit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,115 +1,216 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { useEffect, useMemo, useState } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const PRODUCTS = [
+  { id: 1, name: "Laptop", price: 500 },
+  { id: 2, name: "Smartphone", price: 300 },
+  { id: 3, name: "Headphones", price: 100 },
+  { id: 4, name: "Smartwatch", price: 150 },
+];
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const FREE_GIFT = { id: 99, name: "Wireless Mouse", price: 0 };
+const THRESHOLD = 1000;
+
+const QuantitySelector = ({ item, onChange, onAdd, onRemove, width }) => {
+  return (
+    <div className={`flex`}>
+      <button
+        className={`bg-red-500 text-white w-6 h-6 rounded-sm ${
+          width === "full" && "w-10 h-10"
+        }`}
+        onClick={() => onRemove(item)}
+      >
+        -
+      </button>
+      <input
+        type="number"
+        className={`outline-none focus:ring-2 ring-offset-2 rounded-md w-6 text-center mx-1 ${
+          width === "full" && "grow"
+        }`}
+        value={item.quantity}
+        onChange={(e) => onChange(e, item)}
+      />
+      <button
+        className={`bg-green-500 text-white w-6 h-6 rounded-sm ${
+          width === "full" && "w-10 h-10"
+        }`}
+        onClick={() => onAdd(item)}
+      >
+        +
+      </button>
+    </div>
+  );
+};
 
 export default function Home() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const onChangeProductQuantity = (e, product) => {
+    let { value: quantity } = e.target;
+
+    if (quantity < 1) {
+      quantity = 0;
+    }
+
+    setCartItems((prev) => {
+      if (quantity === 0) {
+        return prev.filter((x) => x.id !== product.id);
+      } else {
+        return prev.map((x) =>
+          x.id === product.id ? { ...x, quantity: quantity } : x
+        );
+      }
+    });
+  };
+
+  const addProductToCart = (product) => {
+    setCartItems((prev) => {
+      const productIndex = prev.findIndex((x) => x.id === product.id);
+
+      if (productIndex !== -1) {
+        return prev.map((x, i) =>
+          i === productIndex ? { ...x, quantity: x.quantity + 1 } : x
+        );
+      } else {
+        return [...prev, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  const removeProductToCart = (product) => {
+    setCartItems((prev) => {
+      const productQuantity = prev.find((x) => x.id === product.id)?.quantity;
+
+      if (productQuantity === 1) {
+        return prev.filter((x) => x.id !== product.id);
+      } else {
+        return prev.map((x) =>
+          x.id === product.id ? { ...x, quantity: x.quantity - 1 } : x
+        );
+      }
+    });
+  };
+
+  const totalValue = useMemo(() => {
+    return cartItems.reduce((sum, x) => {
+      return sum + x.price * x.quantity;
+    }, 0);
+  }, [cartItems]);
+
+  useEffect(() => {
+    if (totalValue < THRESHOLD) {
+      setCartItems((prev) => prev.filter((x) => x.id !== FREE_GIFT.id));
+    } else {
+      if (!cartItems.some((x) => x.id === FREE_GIFT.id)) {
+        setCartItems((prev) => [...prev, { ...FREE_GIFT, quantity: 1 }]);
+      }
+    }
+  }, [totalValue]);
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="container mx-auto my-8">
+      <h1 className="text-4xl font-extrabold text-center">Shopping Cart</h1>
+      <div className="mb-6">
+        <h2 className="text-2xl font-bold mb-2">Products</h2>
+        <div className="grid grid-cols-12 gap-4">
+          {PRODUCTS.map((product) => {
+            const cartItem = cartItems.find((x) => x.id === product.id);
+
+            return (
+              <div
+                key={product.id}
+                className="col-span-3 bg-white rounded-md shadow-md p-3"
+              >
+                <h3 className="font-bold mb-2">{product.name}</h3>
+                <p className="mb-2">${product.price}</p>
+                {!!cartItem ? (
+                  <QuantitySelector
+                    item={cartItem}
+                    onChange={onChangeProductQuantity}
+                    onAdd={addProductToCart}
+                    onRemove={removeProductToCart}
+                    width="full"
+                  />
+                ) : (
+                  <button
+                    className="bg-blue-500 hover:bg-blue-600 text-white rounded-md py-2 w-full cursor-pointer"
+                    onClick={() => addProductToCart(product)}
+                  >
+                    Add to Cart
+                  </button>
+                )}
+              </div>
+            );
+          })}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="mb-4">
+        <h2 className="text-2xl font-bold mb-2">Cart Summary</h2>
+        <div className="bg-white rounded-md shadow-md p-4">
+          <div className="flex items-center justify-between py-2 border-b-1 mb-3">
+            <span className="font-bold">Subtotal:</span>
+            <span className="font-extrabold">${totalValue}</span>
+          </div>
+          {totalValue < THRESHOLD ? (
+            <div className="bg-blue-100 rounded-md p-3">
+              <div className="mb-2">
+                Add ${THRESHOLD - totalValue} more to get a FREE Wireless Mouse!
+              </div>
+              <div className="relative">
+                <div className="bg-gray-300 h-4 rounded-full"></div>
+                <div
+                  className={`absolute bg-blue-500 rounded-full h-4 top-0 transition-all`}
+                  style={{ width: `${(totalValue * 100) / THRESHOLD}%` }}
+                ></div>
+              </div>
+            </div>
+          ) : (
+            <div>You got a free {FREE_GIFT.name}!</div>
+          )}
+        </div>
+      </div>
+      <div className=" mb-4">
+        {cartItems.length !== 0 && (
+          <h2 className="text-2xl font-bold mb-2">Cart Items</h2>
+        )}
+        {!!cartItems.length ? (
+          <>
+            {cartItems.map((item) => (
+              <div
+                key={item.id}
+                className="bg-white rounded-md shadow-md flex flex-col items-center justify-center p-4 mb-3"
+              >
+                <div className="w-full flex items-center justify-center">
+                  <div className="grow">
+                    <div className="mb-0.5">{item.name}</div>
+                    <div className="text-gray-500">
+                      ${item.price} x {item.quantity}
+                    </div>
+                  </div>
+                  {item.id !== FREE_GIFT.id ? (
+                    <QuantitySelector
+                      item={item}
+                      onChange={onChangeProductQuantity}
+                      onAdd={addProductToCart}
+                      onRemove={removeProductToCart}
+                    />
+                  ) : (
+                    <span className="bg-green-200 text-green-700 text-sm px-3 py-1 rounded-full">
+                      FREE GIFT
+                    </span>
+                  )}
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <div className="bg-white rounded-md shadow-md flex flex-col items-center justify-center py-6 px-4">
+            <div className="text-gray-600 text-lg font-semibold mb-2">
+              Your Cart is Empty
+            </div>
+            <p className="text-gray-400">Add some products to see them here!</p>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
